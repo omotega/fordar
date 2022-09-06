e0998124703ce1b6f9b118b8310534ffe2383e71
omotega <tomoyibo@gmail.com>
2022-09-06 20:52:04 +0100

    updated vendor routes

--- src/route/adminroute.js
+++ src/route/adminroute.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const adminRouter = express.Router();
 
-const { findVendor, createVendor } = require('../controller/admincontroller');
+const { findVendor, createVendor,getAllVendors,getVendorById } = require('../controller/admincontroller');
 const { adminauth } = require('../middlewares/adminmiddlewre');
 
 adminRouter.route('/findvendor').get(adminauth, findVendor);
-adminRouter.route('/vendor').post(adminauth, createVendor);
+adminRouter.route('/vendor').post(adminauth, createVendor)
+adminRouter.route('/vendors').get(getAllVendors);
+adminRouter.route('/vendor/:id').get(getVendorById);
 
 module.exports = adminRouter;
